6faef3c84afb5631c9727790bc29f364f5c92914
nguyen9x97 <nguyen9x999@gmail.com>
2022-01-02 21:04:13 +0700

    Fixed uri: postgres to postgresql

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ from db import db
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
